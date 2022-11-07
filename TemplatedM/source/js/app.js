91cb0fa39aa23821de14e67ee27fe49c715c2c5e
ElyaneMehiyeva <elyamehiyeva@gmail.com>
2022-11-07 11:14:26 -0800

    Template

---
+++ TemplatedM/source/js/app.js
@@ -0,0 +1,16 @@
+var navLinks = document.querySelectorAll("nav>a")
+
+navLinks.forEach(link=>{
+    link.addEventListener("click",function(e){
+        e.preventDefault()
+        clearClassList(navLinks)
+        link.className = "active"
+    })
+})
+
+
+function clearClassList(links){
+    links.forEach(link=>{
+        link.className = ""
+    })
+}
